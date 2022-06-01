3394b5565c61e039cc078a4531a8f4810b8742cd
GIHAA <gihansudeeparandimal@gmail.com>
2022-06-01 16:35:41 +0530

    preloader working

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,24 @@
-
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Preloader1 from "./components/Preloader1";
+import React, { useEffect, useState } from "react";
+import ReactLoading from 'react-loading';
 
 function App() {
+  const [done , setDone] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+        setDone(true);
+    }, 2000);
+  },[])
+
   return (
-    <div className="text-3xl">
-      <h1>hello gihan</h1>
+    <div className="text-white">
+      {/* <Preloader1 /> */}
+      { !done ? <Preloader1/> : <><Navbar /><Home /></>  }
+      {/* <Navbar />
+      <Home /> */}
     </div>
   );
 }
---
+++ portfolio/src/components/Preloader1.jsx
@@ -0,0 +1,12 @@
+
+import ReactLoading from 'react-loading';
+
+const Preloader1 = () => {
+
+  return (
+  <ReactLoading type={"bars"} color={"red"} height={667} width={375} />
+
+  )
+}
+
+export default Preloader1;
